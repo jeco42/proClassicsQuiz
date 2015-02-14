5d38d27cef309825163739ac6872a8f519dc5ecc
jeco42 <jeco42@gmail.com>
2015-02-14 11:29:53 -0500

    main screen layout added

--- app/src/main/java/com/weebley/jeco42/proclassicsquiz/MainActivity.java
+++ app/src/main/java/com/weebley/jeco42/proclassicsquiz/MainActivity.java
@@ -19,9 +19,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
         }
     }
 
